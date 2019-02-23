8926cb3eb3218b605bb46a52379af9cecd33a0d8
ahatton <andrewjhatton@hotmail.co.uk>
2019-02-23 21:44:52 +0000

    db question 3 completed

--- sql/answers/customersSecondHighestOrderValue.sql
+++ sql/answers/customersSecondHighestOrderValue.sql
@@ -1,8 +1,3 @@
-SELECT o1.*
-FROM orders o1
-       JOIN (SELECT customer_id, MAX(price) max_price FROM orders GROUP BY customer_id) o2
-            ON o1.customer_id = o2.customer_id AND o1.price = o2.max_price;
-
 SELECT customer_id, MAX(price) second_highest_price
 FROM orders
 WHERE order_id NOT IN (SELECT o1.order_id
