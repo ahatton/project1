1d1e7b7be15216663b4123a18da62d46b6e5ea4f
ahatton <andrewjhatton@hotmail.co.uk>
2019-02-24 15:59:10 +0000

    Adding extra test data to schema

--- sql/docker/db/schema.sql
+++ sql/docker/db/schema.sql
@@ -4,9 +4,16 @@ GRANT ALL PRIVILEGES ON mds.* TO 'mds'@'%';
 
 USE mds;
 CREATE TABLE orders (order_id numeric, price numeric, destination varchar(255), customer_id varchar(255));
+
+-- Standard test data
 INSERT INTO orders VALUES (23, 800, 'Cork', 'ebz');
 INSERT INTO orders VALUES (24, 200, 'Dublin', 'xyz');
 INSERT INTO orders VALUES (26, 150, 'Mayo', 'ebz');
 INSERT INTO orders VALUES (33, 700, 'Cork', 'cbc');
 INSERT INTO orders VALUES (34, 25, 'Cork', 'cbc');
 INSERT INTO orders VALUES (35, 50, 'Limerick', 'xyz');
+
+-- Extra test data
+INSERT INTO orders VALUES (76, 5, 'London', 'xyz');
+INSERT INTO orders VALUES (87, 10, 'Paris', 'cbc');
+INSERT INTO orders VALUES (98, 15, 'Berlin', 'ebz');
